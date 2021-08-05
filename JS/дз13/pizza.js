d249dff188dbaf5998141afa322cd461197fae0a
Nastay Mandrychenko <nastyamarichenko@gmail.com>
2021-08-05 17:33:50 +0200

    pizza html finished

---
+++ JS/дз13/pizza.js
@@ -0,0 +1,15 @@
+// window.onload = function () {
+//     let result;
+//     // document.getElementById("result").innerHTML = result;
+//     let mushrooms = document.getElementById("mushrooms")
+
+//     mushrooms.addEventListener("click", function () {
+//         result += 5
+//         document.getElementById("result").innerHTML = result;
+//     }, false)
+// }
+let result;
+function mushrooms(){
+    result = 0 + 5
+    document.getElementById("result").innerHTML = result;
+}
